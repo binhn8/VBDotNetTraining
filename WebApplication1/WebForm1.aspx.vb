cff64fd2c7294652e7087a673cd8982d2c0620a2
binh <bttt0808@gmail.com>
2015-06-16 23:10:54 +0100

    This is first commit to test

---
+++ WebApplication1/WebForm1.aspx.vb
@@ -0,0 +1,13 @@
+﻿Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not Page.IsPostBack Then
+            lblLoading.Text = "Hello, I'm Binh. I'm practicing VB.NET"
+        End If
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Response.Write("Hello every body")
+    End Sub
+End Class
